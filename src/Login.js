7fb3a35c157144223380ec6025acf95316872b30
hillhousedev <adesemoyeolusola@gmail.com>
2020-12-03 18:19:05 +0100

    Added a login file and also cleaned the existing files

---
+++ src/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Login() {
+
+  return (
+
+    <div>
+        {/* Spotify logo */}
+        {/* Login with Spotify button */}
+    </div>
+  )
+
+}
+
+
+export default Login
